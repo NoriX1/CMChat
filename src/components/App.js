9476ae0fa7ae2227b6b9d53b0055b34120e5d181
NoriX <versatility.95@mail.ru>
2019-09-09 15:17:58 +0300

    startup config

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div className="container">
+            <div>I'm the app</div>
+        </div>
+    );
+}
+
+export default App;
