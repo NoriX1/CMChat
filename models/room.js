5dbd5782e36101ac2b78120bd0e50453cf74261f
NoriX <versatility.95@mail.ru>
2019-10-27 12:28:18 +0300

    Room model includes fields for private rooms

--- models/room.js
+++ models/room.js
@@ -1,10 +1,36 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt-nodejs');
 const Schema = mongoose.Schema;
 
 const roomSchema = new Schema({
     name: { type: String, unique: true },
-    _owner: { type: Schema.Types.ObjectId, ref: 'user' }
+    _owner: { type: Schema.Types.ObjectId, ref: 'user' },
+    isPrivate: { type: Boolean, default: false },
+    password: String
 });
+
+roomSchema.pre('save', function (next) {
+    const room = this;
+    if (room.isPrivate && !room.password) { return next(); };
+    bcrypt.genSalt(10, function (err, salt) {
+        if (err) { return next(err); }
+
+        bcrypt.hash(room.password, salt, null, function (err, hash) {
+            if (err) { return next(err); }
+
+            room.password = hash;
+            next();
+        });
+    });
+});
+
+roomSchema.methods.comparePassword = function (candidatePassword, callback) {
+    bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
+        if (err) { return callback(err); }
+
+        callback(null, isMatch);
+    });
+}
 
 const ModelClass = mongoose.model('rooms', roomSchema);
 
